3e84d3ee04576372072a24eb511e6fd02341eff5
riceel527-hash <riceel527@gmail.com>
2026-08-10 13:25:41 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-//class App extends React.Component {
-  //render() {
-    //return (
-      // <h1>YouTube Clone App</h1> 
-    //)
-  //}
-//}
-
-const App = () => {
-  return (
-    <h1>YouTube Clone App</h1>
-  )
+class App extends React.Component {
+  render() {
+    return (
+       <h1>YouTube Clone App</h1> 
+    )
+  }
 }
+
+
 
 export default App;
 
