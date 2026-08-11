f10bc5f8f09b21c96e1849d11cd3ade0a26d6b5d
riceel527-hash <riceel527@gmail.com>
2026-08-11 10:57:04 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import{ Grid } from '@material-ui/core';
 
 import youtube from './api/youtube';
 
+import { SearchBar, VideoList, VideoDetails } from './components';
+
 class App extends React.Component {
   render() {
     return (
-       <Grid container spacing={16}>
-
-
-        
-       </Grid>
+       <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+            <Grid container spacing={16}>
+                <Grid item xs={12}>
+                    {/* Search Bar */}
+                 </Grid>
+                <Grid item xs={8}>
+                  {/* Video Details */}
+                </Grid>
+                <Grid item xs={4}>
+                    {/* Video List */}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
   }
 }
@@ -19,5 +31,7 @@ class App extends React.Component {
 
 
 export default App;
+
+
 
 
