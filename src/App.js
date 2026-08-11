4d8911389fabda936365000f20e513424312dc7f
riceel527-hash <riceel527@gmail.com>
2026-08-11 10:00:30 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import{ Grid } from '@material-ui/core';
+
+import youtube from './api/youtube';
+
 class App extends React.Component {
   render() {
     return (
-       <h1>YouTube Clone App</h1> 
+       <Grid container spacing={16}>
+
+
+        
+       </Grid>
     )
   }
 }
